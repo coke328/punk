fa8dcddf58d375ff54deedbe1943009aa80cad85
kyumin <kyuminhan823@gmail.com>
2023-09-06 21:18:09 +0900

    열쇠 시스템

--- Assets/script/PlayerDataScriptableObject.cs
+++ Assets/script/PlayerDataScriptableObject.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "playerData",menuName ="ScriptableObject/playerData")]
 public class PlayerDataScriptableObject : ScriptableObject
 {
-    public Dictionary<string,int> key;//열쇠의 이름과 갯수
+
+    public List<key> keys = new List<key>();//열쇠 리스트
     public int coin;
     public int health;
     public string sceneName;
@@ -13,4 +16,58 @@ public class PlayerDataScriptableObject : ScriptableObject
     public void setSceneName(string name){
         sceneName = name;
     }
+    public void getKey(string name,int cnt){
+        int idx = containsKey(name);
+        if(idx != -1){
+            keys[idx].keyCount += cnt;
+        }else{
+            keys.Add(new key(name,cnt));
+        }
+    }
+    public int containsKey(string keyName){
+        for(int i=0; i<keys.Count; i++){
+            if(keys[i].keyName == keyName){//key를 찾으면 인덱스를 리턴하고,못찾으면 -1 리턴
+                return i;
+            }
+        }
+        return -1;
+    }
+    public bool containsKeyOver(string name,int cnt){//키를 찾아서 일정 갯수 이상일때 true 리턴
+        int idx = containsKey(name);
+        if(idx != -1){
+            if(keys[idx].keyCount >= cnt){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void removeKey(string name,int cnt){
+        int idx = containsKey(name);
+        if(idx != -1){
+            if(keys[idx].keyCount>cnt){
+                keys[idx].keyCount -= cnt;
+            }else{
+                keys.RemoveAt(idx);
+            }
+        }
+    }
+    public void resetKey(){
+        keys.Clear();
+    }
+    public void keyPrint(){
+        foreach(key k in keys){
+            Debug.Log(k.keyName + " : " + k.keyCount);
+        }
+    }
+}
+
+[System.Serializable]
+public class key
+{
+    public key(string name,int count){
+        this.keyName = name;
+        this.keyCount = count;
+    }
+    public string keyName;
+    public int keyCount;
 }
--- Assets/script/respawn.cs
+++ Assets/script/respawn.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -19,14 +21,11 @@ public class respawn : MonoBehaviour
             lastData.deadCnt = currentData.deadCnt;
         }else{
             //새 스테이지에 왔을때
-            lastData = currentData;
+            lastData.coin = currentData.coin;
+            lastData.health = currentData.health;
+            //lastData.keys = currentData.keys.ToList<key>();//얕은 복사
+            lastData.keys = currentData.keys.ConvertAll(k => new key(k.keyName,k.keyCount));//깊은 복사
             lastData.setSceneName(SceneManager.GetActiveScene().name);
-        }
-    }
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Q)){
-            hpScript.damage(5);//리스폰시키기
         }
     }
 
@@ -34,17 +33,35 @@ public class respawn : MonoBehaviour
     public void loadLastData(){//리스폰 할때 Hp스크립트 dead이벤트랑 연결 ,죽으면 씬 로딩 다시
         currentData.coin = lastData.coin;//스테이지 시작할때의 데이터를 현재 데이터에 저장
         currentData.health = lastData.health;
-        currentData.key = lastData.key;
+        //currentData.keys = lastData.keys.ToList<key>();//얕은 복사
+        currentData.keys = lastData.keys.ConvertAll(k => new key(k.keyName,k.keyCount));//깊은 복사
         currentData.deadCnt++;//죽은 횟수 추가
         SceneManager.LoadScene(lastData.sceneName);
     }
     public void getKey(string keyName,int cnt){//열쇠를 얻었을때
-        currentData.key.Add(keyName,cnt);
+        currentData.getKey(keyName,cnt);
     }
     public void addCoin(int cnt){//코인 얻음
         currentData.coin += cnt;
     }
     public void setHealth(){//체력지정 Hp스크립트랑 연결(체력을 잃거나 얻었을떄 동기화)
         currentData.health = hpScript.Hp;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q)){
+            hpScript.damage(5);//데미지 입히기
+        }
+        if(Input.GetKeyDown(KeyCode.K)){//현재가진 열쇠 콘솔로 띄우기
+            Debug.Log("current");
+            currentData.keyPrint();
+            Debug.Log("last");
+            lastData.keyPrint();
+        }
+        if(Input.GetKeyDown(KeyCode.R)){//열쇠 초기화
+            currentData.resetKey();
+            lastData.resetKey();
+        }
     }
 }
