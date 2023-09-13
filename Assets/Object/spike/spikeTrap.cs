71a63b60680837d983da6936a6bf3b956a8b948a
Electroncoffee <electroncoffee@kangwon.ac.kr>
2023-09-14 00:22:38 +0900

    스파이크 트랩 & 콘베이어 복구

--- Assets/Object/spike/spikeTrap.cs
+++ Assets/Object/spike/spikeTrap.cs
@@ -5,34 +5,37 @@ using UnityEngine.Events;
 
 public class spikeTrap : MonoBehaviour
 {
+    public int damage;
     public float AppearTime;
     public float DisAppearTime;
     private BoxCollider2D bc;
     private SpriteRenderer sr;
-    public UnityEvent Spikehit;
+    private Animator anim;
+    public UnityEvent<int> Spikehit;
     // Start is called before the first frame update
     void Start()
     {
         bc = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         StartCoroutine(Toggle());
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
-            Spikehit.Invoke();
+            Spikehit.Invoke(damage);
         }
     }
     private IEnumerator Toggle()
     {
         while (true)
         {
-            bc.enabled = true;
-            sr.color = Color.white;
-            yield return new WaitForSeconds(AppearTime);
             bc.enabled = false;
-            sr.color = Color.black;
+            anim.Play("Down_");
+            yield return new WaitForSeconds(AppearTime);
+            bc.enabled = true;
+            anim.Play("Up_");
             yield return new WaitForSeconds(DisAppearTime);
         }
     }
